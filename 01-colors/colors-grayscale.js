1d143f57c2a53aa3408514e3095bd93554577c42
Brian Lewis <brian@modulesunraveled.com>
2020-04-15 15:34:01 -0500

    Created colors stories.

---
+++ 01-colors/colors-grayscale.js
@@ -0,0 +1,21 @@
+const grayscale = [
+  'white',
+  'gray-100',
+  'gray-200',
+  'gray-300',
+  'gray-400',
+  'gray-500',
+  'gray-600',
+  'gray-700',
+  'gray-800',
+  'gray-900',
+  'gray-transparent-100',
+  'gray-transparent-200',
+  'gray-transparent-300',
+  'gray-transparent-400',
+  'gray-transparent-500',
+  'gray-transparent-600',
+  'black',
+];
+
+export default grayscale;
---
+++ 01-colors/colors.stories.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// import Colors from './colors';
+
+import grayscale from './colors-grayscale.js';
+import branding from './colors-branding.js';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Base/Colors' };
+
+// export const grayScale = () => <Colors>React Button</Colors>;
+
+const grayscaleItems = grayscale.map((item) => (
+  <li className="sg-colors__list" key={item}>
+    <div className={`sg-colors__color sg-colors__color--${item}`}></div>
+    <div className="sg-info">
+      <code>{item}</code>
+    </div>
+  </li>
+));
+const brandingItems = branding.map((item) => (
+  <li className="sg-colors__list" key={item}>
+    <div className={`sg-colors__color sg-colors__color--${item}`}></div>
+    <div className="sg-info">
+      <code>{item}</code>
+    </div>
+  </li>
+));
+
+export const Grayscale = () => <ul className="sg-colors">{grayscaleItems}</ul>;
+export const Branding = () => <ul className="sg-colors">{brandingItems}</ul>;
