5245513015327f75ebc8a0f6e7e01149bdb81c7e
Brian Lewis <brian@modulesunraveled.com>
2020-04-23 15:12:06 -0500

    Created initial paragraph component and story.

---
+++ 01-atoms/text/text.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Paragraph from './Paragraph.component';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Atoms/Text' };
+
+export const paragraph = () => <Paragraph>Here's a test paragraph.</Paragraph>;
