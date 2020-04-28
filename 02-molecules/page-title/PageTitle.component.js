9e6b9d52b86fe858918b2d3739d62b58e3b2ac19
Joe Tower <joetower@gmail.com>
2020-04-28 17:09:59 -0500

    adding page title component and story

---
+++ 02-molecules/page-title/PageTitle.component.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../../01-atoms/text/Heading.component';
+import withModifiers from '../../_utils/withModifiers';
+
+const PageTitle = ({ PageTitleModifiers, heading }) => (
+  <div className={withModifiers('page-title page-title__wrapper')(PageTitleModifiers)}>
+    <Header level="1" baseClass="page-title__heading">
+      {heading}
+    </Header>
+  </div>
+);
+
+PageTitle.propTypes = {
+  PageTitleModifiers: PropTypes.arrayOf(PropTypes.string),
+  heading: PropTypes.string.isRequired,
+};
+
+export default PageTitle;
