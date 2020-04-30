90fb7ff97c8d82f1c1ad672f12809c92d726d7e7
Joe Tower <joetower@gmail.com>
2020-04-30 14:08:09 -0500

    matched markup from twig emulsify

--- 02-molecules/page-title/PageTitle.component.js
+++ 02-molecules/page-title/PageTitle.component.js
@@ -4,8 +4,8 @@ import Header from '../../01-atoms/text/Heading.component';
 import withModifiers from '../../_utils/withModifiers';
 
 const PageTitle = ({ PageTitleModifiers, heading }) => (
-  <div className={withModifiers('page-title page-title__wrapper')(PageTitleModifiers)}>
-    <Header level="1" baseClass="page-title__heading">
+  <div className={withModifiers('page-title__wrapper')(PageTitleModifiers)}>
+    <Header level="1" baseClass="page-title">
       {heading}
     </Header>
   </div>
