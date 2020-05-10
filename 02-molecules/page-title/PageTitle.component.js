c9a655bfcd5de330ffea5e5c6dd7751cb9713965
Joe Tower <joetower@gmail.com>
2020-05-10 15:50:31 -0500

    PR feedback - changed PageTitleModifiers to simply Modifiers

--- 02-molecules/page-title/PageTitle.component.js
+++ 02-molecules/page-title/PageTitle.component.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import Header from '../../01-atoms/text/Heading.component';
 import withModifiers from '../../_utils/withModifiers';
 
-const PageTitle = ({ PageTitleModifiers, heading }) => (
-  <div className={withModifiers('page-title__wrapper')(PageTitleModifiers)}>
+const PageTitle = ({ Modifiers, heading }) => (
+  <div className={withModifiers('page-title__wrapper')(Modifiers)}>
     <Header level="1" baseClass="page-title">
       {heading}
     </Header>
@@ -12,7 +12,7 @@ const PageTitle = ({ PageTitleModifiers, heading }) => (
 );
 
 PageTitle.propTypes = {
-  PageTitleModifiers: PropTypes.arrayOf(PropTypes.string),
+  Modifiers: PropTypes.arrayOf(PropTypes.string),
   heading: PropTypes.string.isRequired,
 };
 
