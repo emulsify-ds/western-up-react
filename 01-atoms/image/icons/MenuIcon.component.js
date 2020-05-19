7edb0f28f8e163d90f3cae70a8963abbafbd10e8
Jim Vomero <jim.vomero@fourkitchens.com>
2020-05-18 20:30:11 -0400

    GS-133: Defining icon components.

---
+++ 01-atoms/image/icons/MenuIcon.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MenuIcon = () => (
+  <svg
+    className='icon'
+    viewBox="0 0 32 32"
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+  >
+    <path d="M2 6h28v6H2zm0 8h28v6H2zm0 8h28v6H2z"/>
+  </svg>
+);
+
+export default MenuIcon;
