97d05eba8439b0233871a4b84e945246a1b68eec
Brian Lewis <brian@modulesunraveled.com>
2020-06-02 13:33:21 -0500

    Created initial card component with stories.

---
+++ 02-molecules/cards/Card/Card.component.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Image from '../../../01-atoms/images/ResponsiveImage/ResponsiveImage.component';
+import { imageTypeProps } from '../../../01-atoms/images/Image/Image.component';
+
+import Heading from '../../../01-atoms/text/Heading.component';
+import Paragraph from '../../../01-atoms/text/Paragraph.component';
+import Button from '../../../01-atoms/buttons/Button.component';
+import bem from '../../../_utils/bem';
+
+const Card = ({
+  outputImage,
+  image,
+  heading,
+  subheading,
+  body,
+  buttonText,
+  cardModifiers,
+}) => {
+  const block = 'card';
+  return (
+    <div className={bem(block, '', cardModifiers)}>
+      {/* Image */}
+      <div className={bem(block, 'image')}>
+        {/* TODO If Image */}
+        <Image outputImage={outputImage} image={image} />
+      </div>
+      {/* Content */}
+      <div className={bem(block, 'content')}>
+        {/* Heading */}
+        {/* TODO If Heading */}
+        <Heading level="2" children={heading} block={block} element="heading" />
+        {/* Subheading */}
+        {/* TODO If Subheading */}
+        <Heading
+          level="3"
+          children={subheading}
+          block={block}
+          element="subheading"
+        />
+        {/* Body */}
+        {/* TODO If body */}
+        <Paragraph children={body} block={block} element="body" />
+        {/* Button */}
+        {/* TODO If button */}
+        <Button children={buttonText} block={block} element="button" />
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  outputImage: PropTypes.bool,
+  image: imageTypeProps,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  body: PropTypes.string,
+  buttonText: PropTypes.string,
+};
+
+export default Card;
---
+++ 02-molecules/cards/cards.stories.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Card from './Card/Card.component';
+// import NewsCard from './news/NewsCard.component';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Molecules/Cards' };
+
+// Component Examples
+export const card = () => (
+  <Card
+    outputImage={true}
+    image={{
+      alt: 'Random Image from Picsum',
+      src: 'https://picsum.photos/400/300.jpg',
+    }}
+    heading="This is the card heading"
+    subheading="Curabitur non nulla sit amet nisl tempus convallis quis ac lectus"
+    body="Ellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a."
+    buttonText="Read more about this card"
+  />
+);
+export const cardBg = () => (
+  <Card
+    cardModifiers={['bg']}
+    outputImage={true}
+    image={{
+      alt: 'Random Image from Picsum',
+      src: 'https://picsum.photos/400/300.jpg',
+    }}
+    heading="This is the card heading"
+    subheading="Curabitur non nulla sit amet nisl tempus convallis quis ac lectus"
+    body="Ellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a."
+    buttonText="Read more about this card"
+  />
+);
+// export const newsCard = () => (
+//   <NewsCard
+//     image_src="https://picsum.photos/1280/720"
+//     heading="This is the card heading"
+//     subheading="Curabitur non nulla sit amet nisl tempus convallis quis ac lectus"
+//     body="Ellentesque in ipsum id orci porta dapibus. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a."
+//     link_text="Read more about this card"
+//     date="2119-12-09 12:00:00"
+//   />
+// );
