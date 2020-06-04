5a3cfa83467e1a7e8ff0428509ecb01ee467a960
Brian Lewis <brian@modulesunraveled.com>
2020-06-04 15:50:32 -0500

    Updated BackgroundImage component to use new methodology.

--- 01-atoms/images/BackgroundImage/BackgroundImage.component.js
+++ 01-atoms/images/BackgroundImage/BackgroundImage.component.js
@@ -4,54 +4,35 @@ import Image from '../../../01-atoms/images/ResponsiveImage/ResponsiveImage.comp
 import { imageTypeProps } from '../Image/Image.component';
 import bem from '../../../_utils/bem';
 
-const BackgroundImage = ({ outputImage, image, children }) => {
-  const block = 'react-background-image';
-
+const BackgroundImage = ({
+  outputImage,
+  image,
+  children,
+  block = 'background-image',
+}) => {
   return (
-    <>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
-            [data-react-background-image] {
-              position: relative;
-              overflow: hidden;
-            }
-            [data-react-background-image__image] {
-              position: absolute;
-              height: 100%;
-              width: 100%;
-            }
-            [data-react-background-image__image] img {
-              height: 100%;
-              width: 100%;
-              object-fit: cover;
-            }
-            [data-react-background-image__content] {
-              position: relative;
-            }
-          `,
-        }}
-      ></style>
-      <div className={bem(block)} data-react-background-image>
-        {/* Image */}
-        <div className={bem(block, 'image')} data-react-background-image__image>
-          {image && <Image outputImage={outputImage} image={image} />}
-        </div>
-        {/* Content */}
+    <div className={bem(block)} data-react-background-image>
+      {/* Image */}
+      <div className={bem(block, 'image')} data-react-background-image__image>
+        {image && <Image outputImage={outputImage} image={image} />}
+      </div>
+      {/* Content */}
+      {children && (
         <div
           className={bem(block, 'content')}
           data-react-background-image__content
         >
           {children}
         </div>
-      </div>
-    </>
+      )}
+    </div>
   );
 };
 
 BackgroundImage.propTypes = {
   outputImage: PropTypes.bool,
   image: imageTypeProps.isRequired,
+  block: PropTypes.string.isRequired,
 };
 
 export default BackgroundImage;
